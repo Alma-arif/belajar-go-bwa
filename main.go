a9ce41c225cc30a1ca49a0a5a8f64bc0aab19d61
Alma-arif <191240000922@unisnu.ac.id>
2022-07-02 13:37:06 +0700

    campaign repository

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"membuat-api-bwa/auth"
+	"membuat-api-bwa/campaign"
 	"membuat-api-bwa/handler"
 	"membuat-api-bwa/helper"
 	"membuat-api-bwa/user"
@@ -25,6 +27,22 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
